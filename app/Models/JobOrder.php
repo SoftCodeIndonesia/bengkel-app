2bf3d52db80741e345bb89122918153f6d481f56
faizal <faizalfernandy08@gmail.com>
2025-08-05 00:46:26 +0700

    JO, Employee

--- app/Models/JobOrder.php
+++ app/Models/JobOrder.php
@@ -51,7 +51,7 @@ class JobOrder extends Model
 
     public function orderItems()
     {
-        return $this->hasMany(OrderItem::class, 'order_id');
+        return $this->hasMany(OrderItem::class, 'order_id')->withTrashed();
     }
 
     public function invoice()
