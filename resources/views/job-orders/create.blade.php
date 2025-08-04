9e1b0704ab8abf3fb85ae65fae6caa944df1fc66
faizal <faizalfernandy08@gmail.com>
2025-08-04 23:30:00 +0700

    fix 4 agust

--- resources/views/job-orders/create.blade.php
+++ resources/views/job-orders/create.blade.php
@@ -104,8 +104,7 @@
 
 
                 <div class=" mb-5">
-                    <label for="package" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Paket
-                        Service</label>
+                    <label for="package" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tipe</label>
                     <select id="package"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="custom" selected>Custom</option>
